cc562885ba083cc10466312a2e1148140a31b7cd
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2014-07-05 23:16:50 +0800

    Revert to re.typed group-id. Bump core.typed.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject com.ambrosebs/re.typed "0.1.4"
+(defproject re.typed "0.1.4"
   :description "regular expressions without a big fat union."
   :url "http://github.com/brehaut/re.typed/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/core.typed "0.2.53"]]
+                 [org.clojure/core.typed "0.2.55"]]
   :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"})
